c8236f9ca2d197c1eb0b17aa012e835d5a6e5440
regaugetswe <regaugetswekekana@gmail.com>
2019-06-06 14:11:54 +0200

    ToDoList

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'example';
-}
+    title ="TO DO LIST"
+    tasks=[];
+    newTask:string= '';
+
+    addTask(){
+      console.log("addtask clickeds");
+     this.tasks.push(this.newTask);
+   
+
+    }
+       deleteTask(task){
+       let index = this.tasks.indexOf(task)
+       this.tasks.splice(index,1)
+     }
+
+     
+   
+ 
+  }
+
+
+
+
+  
+
