d28c86da5ea4c88d15b5b1d2c4d7e82a4c983466
regaugetswe <regaugetswekekana@gmail.com>
2019-06-12 12:07:06 +0200

    myUpdatedoDoList

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,20 +8,45 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
     title ="TO DO LIST"
-    tasks=[];
-    newTask:string= '';
-
-    addTask(){
-      console.log("addtask clickeds");
-     this.tasks.push(this.newTask);
-   
-
+    tasks=[]
+    newTask:string= " ";
+    newPriority:string =" "; 
+    empty = true
+    
+    addTask(){  
+      this.tasks.push({taskName:this.newTask, priority:this.newPriority}) 
+      this.empty=false
+     this.newTask =" ";
+     this.newPriority=" "; 
+     if(this.tasks.length >0){
+      this.empty =false
+     }
+     
     }
        deleteTask(task){
        let index = this.tasks.indexOf(task)
        this.tasks.splice(index,1)
+       if(this.tasks.length <=0){
+        this.empty =true
      }
+    }
 
+     editTask(task){
+     let index = this.tasks.indexOf(task)
+     let newTask = prompt("editTask")
+     let newPri = prompt("edit your priority")
+     if((newTask != null) &&(newPri !=null)){
+       this.tasks[index].taskName = newTask;
+       this.tasks[index].priority=newPri;
+     }
+     
+      
+      }
+      
+     
+    
+
+    
      
    
  
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+
+
 
 @NgModule({
   declarations: [
@@ -11,9 +17,15 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+// export class MyOwnCustomMaterialModule { }
